6c3a482104ac0b57df49406542cdae3bcee36b0b
Tizzycek <tizianoceccon@outlook.it>
2025-10-13 19:16:14 +0200

    Aggiornamento ASCIIVideoPlayer

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ASCIIVideoPlayer/main.cpp
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ASCIIVideoPlayer/main.cpp
@@ -29,56 +29,41 @@ using namespace std;
 atomic<bool> stop_requested(false);
 
 // Controllo del file
-bool load_info(string& frame_path, string& music_path, unsigned int& fps) {
+void load_info(string& frame_path, string& music_path, unsigned int& fps) {
     ifstream file(FILE);
 
     // 1. Apertura
-    if (!file.is_open()) {
-        cerr << "Errore: Impossibile aprire il file di configurazione" << endl;
-        return false;
-    }
+    if (!file.is_open())
+        throw runtime_error("Impossibile aprire il file di configurazione");
 
     // 2. Lettura frame_path (prima riga)
-    if (!getline(file, frame_path)) {
-        cerr << "Errore: Lettura del percorso dei frame fallita" << endl;
-        return false;
-    }
+    if (!getline(file, frame_path))
+        throw runtime_error("Lettura del percorso dei frame fallita");
 
     // 3. Lettura music_path (seconda riga)
-    if (!getline(file, music_path)) {
-        cerr << "Errore: Lettura del percorso della musica fallita" << endl;
-        return false;
-    }
+    if (!getline(file, music_path))
+        throw runtime_error("Lettura del percorso della musica fallita");
 
     // 4. Lettura FPS (terza riga) con controllo numerico
     string fps_str;
-    if (!getline(file, fps_str)) {
-        cerr << "Errore: Lettura degli FPS fallita" << endl;
-        return false;
-    }
+    if (!getline(file, fps_str))
+        throw runtime_error("Lettura degli FPS fallita");
 
     try {
         fps = stoi(fps_str);  // Converte stringa a intero
     } catch (const invalid_argument&) {
-        cerr << "Errore: Gli FPS devono essere un numero valido (rilevato '" << fps_str << "')" << endl;
-        return false;
+        throw runtime_error("Gli FPS devono essere un numero valido (rilevato '" + fps_str + "')");
     } catch (const out_of_range&) {
-        cerr << "Errore: Valore FPS fuori dal range consentito" << endl;
-        return false;
+        throw runtime_error("Valore FPS fuori dal range consentito");
     }
 
     // 5. Controllo valori sensati
-    if (fps <= 0) {
-        cerr << "Errore: Gli FPS devono essere > 0 (rilevato " << fps << ")" << endl;
-        return false;
-    }
+    if (fps <= 0)
+        throw runtime_error("Gli FPS devono essere > 0 (rilevato " + to_string(fps) + ")");
 
     // 6. Verifica righe extra (opzionale)
-    if (string extra_line; getline(file, extra_line)) {
-        cerr << "Warning: Il file di configurazione contiene righe extra (ignorate)" << endl;
-    }
-
-    return true;
+    if (string extra_line; getline(file, extra_line))
+        cout << "Attenzione: il file di configurazione contiene righe extra (ignorate)" << endl;
 }
 
 #ifdef _WIN32
@@ -124,18 +109,18 @@ int main() {
     string music_path;
     unsigned int fps;
 
-    if (!load_info(frame_path, music_path, fps)) {
-        cerr << "Errore nel caricamento delle configurazioni!" << endl;
+    try {
+        load_info(frame_path, music_path, fps);
+    } catch (exception& e) {
+        cerr << "Errore: " << e.what() << endl;
         return 2;
     }
 
+
     FrameNode* frames = load_frames(frame_path.c_str());
 
-    if (frames == nullptr) {
-        cerr << "Errore allocazione memoria!" << endl;
+    if (frames == nullptr)
         return 1;
-    }
-
 
     thread stopper(monitorStopFile);
 
